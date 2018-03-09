5edaa57ad492060834d648474cd9492cebc15e0c
Holden Altaffer <haltaffer@gmail.com>
2018-03-09 04:51:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,12 +7,17 @@ class Artist
   end
 
   def add_song(song)
-    song = Song.new(name)
     @songs << song
     song.artist = self
   end
 
   def songs
     @songs
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
   end
 end

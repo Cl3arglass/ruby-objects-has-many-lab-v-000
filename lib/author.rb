a224829e35efcd4b08f36d66558cd49af8368cc9
Holden Altaffer <haltaffer@gmail.com>
2018-03-09 05:25:51 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
   attr_accessor :name
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -12,12 +14,18 @@ class Author
 
   def add_post(post)
     @posts << post
+    @@post_count += 1
     post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
+    @@post_count += 1
     post.author = self
+  end
+
+  def self.post_count
+    @@post_count
   end
 end
